a83ab3e9f0e78e26f6231e4cd59685f5c72923e4
Aric <aric@MacBook-Pro-3.local>
2017-05-11 10:49:27 -0500

    setup home route, readme & git ignore

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router');
 
 // App Setup
-
-
+app.use(morgan('combined')); // logging framework
+app.use(bodyParser.json({ type: '*/*' })); // middleware to parse incoming requests into json no matter what the type is, any request incoming will be parsed as json
+router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
